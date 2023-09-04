6b305c6519d6594e72bc8a22002a66c00439d2c7
Rukshesh Talwar <rukshesh.talwar@ibm.com>
2023-09-04 10:55:32 +0530

    added more functionality to interfaces and tsconfig files

--- interfaces.ts
+++ interfaces.ts
@@ -1,6 +1,24 @@
-interface User {
+export interface User {
   name: string;
-  age: number;
+  age?: number;
   id: number;
   email: string;
 }
+
+//Object Destructuring
+let { name: userName, email: userLogin }: User = { name: 'Rukshesh', id: 1, email: 'rukshesh.talwar12@gmail.com' }
+
+//Array Destructuring
+let [user1, user2, ...restUsers]: User[] = [
+  { name: 'Rukshesh', id: 1, email: 'rukshesh.talwar12@gmail.com' },
+  { name: 'John', id: 2, email: 'john.talwar@gmail.com' },
+  { name: 'Himanshi', id: 3, email: 'himanshi.suri@gmail.com' }
+]
+
+console.log(user1);
+console.log(user2);
+console.log(restUsers);
+
+
+// let result = restUsers.filter(user => user.id > 2);
+// console.log(result);
